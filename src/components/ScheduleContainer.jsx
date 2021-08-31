69eff088e8d86088d797601e680d31dcee493020
michelle-mok <michellemokhuihui@gmail.com>
2021-08-31 10:15:49 +0800

    put tabs back

--- src/components/ScheduleContainer.jsx
+++ src/components/ScheduleContainer.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import logo from "../assets/4-MILK.png";
 import { Route, Switch, Link, useRouteMatch, Redirect } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
+import Tab from "react-bootstrap/Tab";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import BatchSchedule from "./BatchSchedule";
@@ -10,12 +13,35 @@ import ftbc3 from "../data/12-04-2021_23-09-2021_BATCH3.json";
 import ftbc4 from "../data/12-07-2021_22-12-2021_BATCH4.json";
 
 function ScheduleContainer() {
+  const [key, setKey] = useState("PTBC1");
   let { path, url } = useRouteMatch();
 
   // pass individual batch json files into BatchSchedule to generate schedules
   return (
     <>
-      <Navbar variant="dark" bg="secondary">
+      <Nav
+        variant="tabs"
+        activeKey={key}
+        onSelect={(k) => setKey(k)}
+        className="mb-3"
+      >
+        <Nav.Item>
+          <Nav.Link eventKey="FTBC3">
+            <Link to={`${url}/ftbc3`}>FTBC 3</Link>
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="FTBC4">
+            <Link to={`${url}/ftbc4`}>FTBC 4</Link>
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="PTBC1">
+            <Link to={`${url}/ptbc1`}>PTBC 1</Link>
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      {/* <Navbar variant="dark" bg="secondary">
         <Container>
           <Navbar.Brand href="#home">
             <img src={logo} alt="company logo"></img>
@@ -32,7 +58,7 @@ function ScheduleContainer() {
             </Nav.Link>
           </Nav>
         </Container>
-      </Navbar>
+      </Navbar> */}
       <Switch>
         <Route exact path={path}>
           <Redirect to={`${path}/ftbc3`} />
