b05750f4d8f16db6b5810dbda528a910841deee9
michelle-mok <michellemokhuihui@gmail.com>
2021-09-24 16:44:30 +0800

    removed ftbc 3 schedule

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import TabsContainer from './components/admin/Tabs';
 import ptbc1 from '../src/data/16-08-2021_15-10-2022_BATCH1.json';
-import ftbc3 from '../src/data/12-04-2021_23-09-2021_BATCH3.json';
 import ftbc4 from '../src/data/12-07-2021_22-12-2021_BATCH4.json';
 import ptbc2 from '../src/data/15-11-2021_21-01-2023_BATCH2.json';
 import ftbc5 from '../src/data/04-10-2021_24-03-2022_BATCH5.json';
@@ -16,7 +15,7 @@ import ScheduleContainer from './components/schedules/ScheduleContainer';
 function App() {
 
   // put all batch data files in an array to be passed into separate components for /shedule and /admin
-  const batchArray = [{name:"ptbc1", content: ptbc1}, {name:"ptbc2", content: ptbc2}, {name: "ftbc3", content: ftbc3}, {name: "ftbc4", content: ftbc4}, {name:"ftbc5", content: ftbc5}];
+  const batchArray = [{name:"ptbc1", content: ptbc1}, {name:"ptbc2", content: ptbc2}, {name: "ftbc4", content: ftbc4}, {name:"ftbc5", content: ftbc5}];
 
   return (
     <div className="App">
