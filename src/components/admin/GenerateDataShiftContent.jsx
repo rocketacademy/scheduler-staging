74bc86a2eac5d28f8fd4da9d591521ed2fa950ce
michelle-mok <michellemokhuihui@gmail.com>
2021-12-12 15:57:59 +0800

    added ptbc to admin section

--- src/components/admin/GenerateDataShiftContent.jsx
+++ src/components/admin/GenerateDataShiftContent.jsx
@@ -5,39 +5,51 @@ import Button from "react-bootstrap/Button";
 import download from "../../download";
 import generateDataObject from "../../generateCourseDates";
 import { DateTime } from "luxon";
+import fulltimeDataFile from "../../data/bootcamp-course-days.json";
+import partTimeDataFile from "../../data/ptbc-course-days.json";
 
 // helper function for cpoying data to clipboard
 const copyToClipboard = (data) => {
   // from stackoverflow, https://stackoverflow.com/questions/58376758/how-to-copy-a-json-data-to-the-clipboard-with-the-button
-  let selBox = document.createElement('textarea');
-  selBox.style.position = 'fixed';
-  selBox.style.left = '0';
-  selBox.style.top = '0';
-  selBox.style.opacity = '0';
+  let selBox = document.createElement("textarea");
+  selBox.style.position = "fixed";
+  selBox.style.left = "0";
+  selBox.style.top = "0";
+  selBox.style.opacity = "0";
   // this copies the JSON data to clipboard with original formatting
   selBox.value = JSON.stringify(data, undefined, 2);
   document.body.appendChild(selBox);
   selBox.focus();
   selBox.select();
-  document.execCommand('copy');
+  document.execCommand("copy");
   document.body.removeChild(selBox);
-}
- 
+};
+
 // ############################################################################
 // ############################################################################
 const GenerateDataShiftContent = ({
   bootcampData,
   setBootcampData,
   mainFile,
+  setMainFile,
   batchFile,
+  mainDays,
+  setMainDays,
   setDaysInBatchFile,
   setDaysInMainFile,
-  batchArray
+  batchArray,
 }) => {
   const [showInputModal, setShowInputModal] = useState(false);
   const [courseDate, setCourseDate] = useState("");
   // used in the close all functionality
   const [accordionKey, setAccordionKey] = useState(1234);
+
+  const displayDataFile = (dataFile) => {
+    console.log("data file", dataFile);
+
+    setMainFile(dataFile);
+    setMainDays(dataFile.days);
+  };
 
   // function that handles download of main data file after edits
   const handleDownloadMainClick = async () => {
@@ -46,21 +58,17 @@ const GenerateDataShiftContent = ({
 
   // function that handles download of individual batch data file after edits
   const handleBatchDownloadClick = () => {
-    download(
-      batchFile,
-      `modified-${batchFile.courseName}-file.json`
-    );
+    download(batchFile, `modified-${batchFile.courseName}-file.json`);
   };
 
   // state set so that rerender occurs
   const handleCloseAll = () => {
     const newAccordionKeyValue = accordionKey + 1;
     setAccordionKey(newAccordionKeyValue);
-  }
+  };
 
   // copys json data file to clipboard
   const handleEditInGithub = (data) => {
-    console.log('data', data);
     copyToClipboard(data);
 
     let gitbookUrl;
@@ -70,34 +78,47 @@ const GenerateDataShiftContent = ({
       gitbookUrl = `https://github.com/rocketacademy/scheduler/edit/main/src/data/${batchFile.courseName}.json`;
     }
     // opens a new window in the browser at specified address(gitbook edit page)
-    window.open(gitbookUrl, "_blank")
-  }
+    console.log("gitbook url", gitbookUrl);
+
+    window.open(gitbookUrl, "_blank");
+  };
 
   const handleBatchEdit = async (index) => {
     try {
       const batch = batchArray[index];
-      const startDate = DateTime.fromFormat(batch.content.courseName.slice(0, 10), 'dd-MM-yyyy').toFormat('yyyy-MM-dd');
+      const startDate = DateTime.fromFormat(
+        batch.content.courseName.slice(0, 10),
+        "dd-MM-yyyy"
+      ).toFormat("yyyy-MM-dd");
       let courseType;
-      if (batch.name.includes('pt')) {
-        courseType = 'Bootcamp PT';
+      if (batch.name.includes("pt")) {
+        courseType = "Bootcamp PT";
       } else {
-        courseType = 'Bootcamp FT';
+        courseType = "Bootcamp FT";
       }
-      const courseName = batch.content.courseName[batch.content.courseName.length-1];
-      const data = await generateDataObject(startDate, courseName, courseType, mainFile);
+      const courseName =
+        batch.content.courseName[batch.content.courseName.length - 1];
+      const data = await generateDataObject(
+        startDate,
+        courseName,
+        courseType,
+        mainFile
+      );
       copyToClipboard(data);
-      window.open(`https://github.com/rocketacademy/scheduler/edit/main/src/data/${data.courseName}.json`, "_blank")
-
-    }
-    catch (error) {
+      window.open(
+        `https://github.com/rocketacademy/scheduler/edit/main/src/data/${data.courseName}.json`,
+        "_blank"
+      );
+    } catch (error) {
       console.log(error);
     }
-  }
-  
+  };
+
   return (
     <>
       {/* renders batch schedule data file  */}
-      {bootcampData.constructor === Object &&
+      {bootcampData &&
+        bootcampData.constructor === Object &&
         Object.keys(bootcampData).length > 0 && (
           <div>
             <div className="download-button-container">
@@ -106,9 +127,8 @@ const GenerateDataShiftContent = ({
                 type="submit"
                 onClick={() => handleEditInGithub(batchFile)}
               >
-                  Edit in GitHub Repo
-              </Button>
-              {" "}
+                Edit in GitHub Repo
+              </Button>{" "}
               <Button
                 variant="primary"
                 type="submit"
@@ -123,21 +143,21 @@ const GenerateDataShiftContent = ({
             {Object.keys(bootcampData).map((day, dayIndex) => {
               return (
                 <>
-                <div>
-                  <MainAccordion
-                    dayIndex={dayIndex}
-                    bootcampData={bootcampData}
-                    setBootcampData={setBootcampData}
-                    day={day}
-                    setShowInputModal={setShowInputModal}
-                    setCourseDate={setCourseDate}
-                    handleCloseAll={handleCloseAll}
-                    accordionKey={accordionKey}
-                    setAccordionKey={setAccordionKey}
-                    setDaysInBatchFile={setDaysInBatchFile}
-                    setDaysInMainFile={setDaysInMainFile}
-                  />
-                </div>
+                  <div>
+                    <MainAccordion
+                      dayIndex={dayIndex}
+                      bootcampData={bootcampData}
+                      setBootcampData={setBootcampData}
+                      day={day}
+                      setShowInputModal={setShowInputModal}
+                      setCourseDate={setCourseDate}
+                      handleCloseAll={handleCloseAll}
+                      accordionKey={accordionKey}
+                      setAccordionKey={setAccordionKey}
+                      setDaysInBatchFile={setDaysInBatchFile}
+                      setDaysInMainFile={setDaysInMainFile}
+                    />
+                  </div>
                 </>
               );
             })}
@@ -156,15 +176,29 @@ const GenerateDataShiftContent = ({
             )}
           </div>
         )}
-      {/* displays main data file  */}
-      {bootcampData.constructor === Array && (
+      {/* displays fulltime or part time data file  */}
+      <div className="data-button-container">
+        <Button
+          className="btn btn-primary"
+          onClick={() => displayDataFile(fulltimeDataFile)}
+        >
+          Full Time
+        </Button>
+        <Button
+          className="btn btn-primary"
+          onClick={() => displayDataFile(partTimeDataFile)}
+        >
+          Part Time
+        </Button>
+      </div>
+      {bootcampData && bootcampData.constructor === Array && mainFile && (
         <div className="accordion-container">
           <div className="download-button-container">
             <Button
               className="btn btn-primary"
               onClick={() => handleEditInGithub(mainFile)}
             >
-            Edit in GitHub Repo
+              Edit in GitHub Repo
             </Button>
             <Button
               className="btn btn-primary"
@@ -174,15 +208,36 @@ const GenerateDataShiftContent = ({
             </Button>
           </div>
           <div className="batchfile-edit-container">
-          {batchArray.map((batch, index) => {
-            return (
-              <>
-              <Button className="batch-update" onClick={() => handleBatchEdit(index)}>
-                Update {batch.name}
-              </Button>
-              </>
-            )
-          })}
+            {batchArray.map((batch, index) => {
+              // different batches are shown depending on whether the data file rendered is the part time or full time one
+              if (mainFile.daysOfWeek.length < 5) {
+                return (
+                  <>
+                    {batch.name.includes("pt") && (
+                      <Button
+                        className="batch-update"
+                        onClick={() => handleBatchEdit(index)}
+                      >
+                        Update {batch.name}
+                      </Button>
+                    )}
+                  </>
+                );
+              } else {
+                return (
+                  <>
+                    {batch.name.includes("ft") && (
+                      <Button
+                        className="batch-update"
+                        onClick={() => handleBatchEdit(index)}
+                      >
+                        Update {batch.name}
+                      </Button>
+                    )}
+                  </>
+                );
+              }
+            })}
           </div>
           <div className="close-all-container">
             <Button onClick={handleCloseAll}>Close All</Button>
@@ -223,4 +278,3 @@ const GenerateDataShiftContent = ({
 };
 
 export default GenerateDataShiftContent;
-
