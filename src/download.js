9b2fbf60821035582530e7b5b5c3e8412252b92a
Kai Yuan Neo <kai@rocketacademy.co>
2022-04-20 23:27:50 +0800

    lint files

--- src/download.js
+++ src/download.js
@@ -1,10 +1,10 @@
 const download = (data, filename) => {
-        // from: https://stackoverflow.com/questions/19721439/download-json-object-as-a-file-from-browser
-        var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data));
-        var dlAnchorElem = document.createElement('a');
-        dlAnchorElem.setAttribute("href",     dataStr     );
-        dlAnchorElem.setAttribute("download", filename);
-        dlAnchorElem.click();
-    };
+  // from: https://stackoverflow.com/questions/19721439/download-json-object-as-a-file-from-browser
+  const dataStr = `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(data))}`;
+  const dlAnchorElem = document.createElement('a');
+  dlAnchorElem.setAttribute('href', dataStr);
+  dlAnchorElem.setAttribute('download', filename);
+  dlAnchorElem.click();
+};
 
 export default download;
