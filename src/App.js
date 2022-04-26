6e9431a358db296ee1ada028592c2df2d072195c
Kai Yuan Neo <kai@rocketacademy.co>
2022-04-26 18:57:57 +0800

    Update M2 for PTBC3 and FTBC7 to learn JSON and space out P2

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import {
 
 import ftbc6 from './data/10-01-2022_01-07-2022_FTBC6.json';
 import ftbc7 from './data/28-03-2022_19-09-2022_FTBC7.json';
+import ftbc7New from './data/28-03-2022_19-09-2022_FTBC7 (v2.1).json';
 import ptbc1 from './data/17-08-2021_26-07-2022_PTBC1.json';
 import ptbc2 from './data/16-11-2021_29-10-2022_PTBC2.json';
 import ptbc3 from './data/12-03-2022_07-03-2023_PTBC3.json';
+import ptbc3New from './data/12-03-2022_07-03-2023_PTBC3 (v2.1).json';
 
 import ScheduleContainer from './components/schedules/ScheduleContainer';
 import TabsContainer from './components/admin/Tabs';
@@ -20,8 +22,10 @@ function App() {
     { name: 'ptbc1', content: ptbc1 },
     { name: 'ptbc2', content: ptbc2 },
     { name: 'ptbc3', content: ptbc3 },
+    { name: 'ptbc3New', content: ptbc3New },
     { name: 'ftbc6', content: ftbc6 },
-    { name: 'ftbc7', content: ftbc7 }];
+    { name: 'ftbc7', content: ftbc7 },
+    { name: 'ftbc7New', content: ftbc7New }];
 
   return (
     <div className="App">
