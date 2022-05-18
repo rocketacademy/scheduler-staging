da5db9baccf38b09f0080b3aafa52b695f797269
neokaiyuan <kai@rocketacademy.co>
2022-05-18 15:54:46 +0800

    Revise algorithms structure

--- src/generateCourseDates.js
+++ src/generateCourseDates.js
@@ -4,9 +4,9 @@ import basicsData from "./data/schedule-templates/basics-schedule-template.json"
 // import ftbcDataJson from "./data/schedule-templates/ftbc1.0-schedule-template.json";
 // import ftbcDataJson from "./data/schedule-templates/ftbc2.0-schedule-template.json";
 import ftbcDataJson from "./data/schedule-templates/ftbc3.0-schedule-template.json";
-import ptbcDataJson from "./data/schedule-templates/ptbc1.0-schedule-template.json";
+// import ptbcDataJson from "./data/schedule-templates/ptbc1.0-schedule-template.json";
 // import ptbcDataJson from "./data/schedule-templates/ptbc2.0-schedule-template.json";
-// import ptbcDataJson from "./data/schedule-templates/ptbc3.0-schedule-template.json";
+import ptbcDataJson from "./data/schedule-templates/ptbc3.0-schedule-template.json";
 
 const { publicHolidays } = holidayData;
 const publicHolidayArray = [];
