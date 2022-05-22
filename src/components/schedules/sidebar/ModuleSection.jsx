aade55eb1a4a387809b6279bb9bfa81ff36e6414
neokaiyuan <kai@rocketacademy.co>
2022-05-22 19:25:02 +0800

    lint modules, make accordion table header text colour white instead of blue

--- src/components/schedules/sidebar/ModuleSection.jsx
+++ src/components/schedules/sidebar/ModuleSection.jsx
@@ -1,41 +1,44 @@
-import React from 'react';
-import Accordion from 'react-bootstrap/Accordion';
-import { scroller } from 'react-scroll';
+import React from "react";
+import Accordion from "react-bootstrap/Accordion";
+import { scroller } from "react-scroll";
 
-function ModuleSection({ 
-                    section, 
-                    index, 
-                    sectionNames, 
-                    coursetype, 
-                    scheduleData 
-                }) {
-    return (
-        <>
-        {section.length > 1 && (
+function ModuleSection({
+  section,
+  index,
+  sectionNames,
+  coursetype,
+  scheduleData,
+}) {
+  return (
+    <>
+      {section.length > 1 && (
         <Accordion.Item eventKey={index}>
-            <Accordion.Header>{sectionNames[index]}</Accordion.Header>
-            <Accordion.Body>
+          <Accordion.Header>{sectionNames[index]}</Accordion.Header>
+          <Accordion.Body>
             {section.map((info) => {
-            const id = `${coursetype}-week-${scheduleData[info.date].courseWeek}-day-${scheduleData[info.date].dayNumber}`;
-    
-                return (
-                <p onClick={() =>
-                scroller.scrollTo(id, {
-                    smooth: true,
-                    offset: -70,
-                    duration: 100,
-                })
-                }
+              const id = `${coursetype}-week-${
+                scheduleData[info.date].courseWeek
+              }-day-${scheduleData[info.date].dayNumber}`;
+
+              return (
+                <p
+                  onClick={() =>
+                    scroller.scrollTo(id, {
+                      smooth: true,
+                      offset: -70,
+                      duration: 100,
+                    })
+                  }
                 >
-                {info.name}
+                  {info.name}
                 </p>
-                )
+              );
             })}
-            </Accordion.Body>
+          </Accordion.Body>
         </Accordion.Item>
-        )}
-        </>
-    )
+      )}
+    </>
+  );
 }
 
-export default ModuleSection
+export default ModuleSection;
--- src/components/schedules/sidebar/Modules.jsx
+++ src/components/schedules/sidebar/Modules.jsx
@@ -1,89 +1,93 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
-import Accordion from 'react-bootstrap/Accordion';
+import Accordion from "react-bootstrap/Accordion";
 import ModuleSection from "./ModuleSection";
 
 // helper function for generating scheduleObjs array and moduleNameArray
 const generatingDataArrays = (
-                              scheduleData, 
-                              day, 
-                              section, 
-                              classtype, 
-                              scheduleUrls, 
-                              scheduleObjs, 
-                              moduleNameArray
-                              ) => {
+  scheduleData,
+  day,
+  section,
+  classtype,
+  scheduleUrls,
+  scheduleObjs,
+  moduleNameArray
+) => {
   if (scheduleData[day].dateTypes[section][classtype].items) {
     scheduleData[day].dateTypes[section][classtype].items.map((item) => {
       // if the item has a url
-      if(item.url && !scheduleUrls.includes(item.url)) {
+      if (item.url && !scheduleUrls.includes(item.url)) {
         // push the url into scheduleUrls
         scheduleUrls.push(item.url);
         // push item name, url and date into scheduleObjs
-        scheduleObjs.push({name: item.name, url: item.url, date: day});
+        scheduleObjs.push({ name: item.name, url: item.url, date: day });
       }
       if (item.url) {
-        const itemUrlArray = item.url.split('/');
+        const itemUrlArray = item.url.split("/");
         // itemUrlArray[3] is used as the heading of each module section
-        // we're filtering out everythign that does not come from the gitbook 
-        if (!moduleNameArray.includes(itemUrlArray[3]) && itemUrlArray[2] === 'bootcamp.rocketacademy.co') {
+        // we're filtering out everythign that does not come from the gitbook
+        if (
+          !moduleNameArray.includes(itemUrlArray[3]) &&
+          itemUrlArray[2] === "bootcamp.rocketacademy.co"
+        ) {
           moduleNameArray.push(itemUrlArray[3]);
         }
       }
-    })
+    });
   }
-}
+};
 
 // helper finction for accessing required items (items in each class of each section of each day)
 const accessingRequiredItems = (
-                              scheduleData, 
-                              day, 
-                              scheduleUrls, 
-                              scheduleObjs, 
-                              moduleNameArray
-                              ) => {
+  scheduleData,
+  day,
+  scheduleUrls,
+  scheduleObjs,
+  moduleNameArray
+) => {
   if (scheduleData[day].dateTypes.module) {
     Object.keys(scheduleData[day].dateTypes)
-    // filtering out module key
-    .filter(section => section !== 'module')
-    .map((section) => {
-      Object.keys(scheduleData[day].dateTypes[section])
-      // filtering out type key
-      .filter(classtype => classtype !== 'type')
-      .map((classtype) => {
-        // if there item array exists in a section, call the function that gets the required data
-        generatingDataArrays(scheduleData, 
-                            day, 
-                            section, 
-                            classtype, 
-                            scheduleUrls, 
-                            scheduleObjs, 
-                            moduleNameArray);
-        
-      })
-    })
+      // filtering out module key
+      .filter((section) => section !== "module")
+      .map((section) => {
+        Object.keys(scheduleData[day].dateTypes[section])
+          // filtering out type key
+          .filter((classtype) => classtype !== "type")
+          .map((classtype) => {
+            // if there item array exists in a section, call the function that gets the required data
+            generatingDataArrays(
+              scheduleData,
+              day,
+              section,
+              classtype,
+              scheduleUrls,
+              scheduleObjs,
+              moduleNameArray
+            );
+          });
+      });
   }
-}
+};
 
 const sortScheduleObjs = (urlObj, moduleName, general, ice, poce) => {
-  const urlModule = urlObj.url.split('/');
+  const urlModule = urlObj.url.split("/");
   // this is the part of the url which we used to get the moduleName
   const urlModuleName = urlModule[3];
   // if the module name in the url is the current moduleName
   if (urlModuleName === moduleName) {
-    const splitName = urlObj.name.split('.');
-    const dataObj = { name: urlObj.name, date: urlObj.date }
+    const splitName = urlObj.name.split(".");
+    const dataObj = { name: urlObj.name, date: urlObj.date };
     // push the object into the relevant array
-    if (splitName[1] === 'ICE') {
+    if (splitName[1] === "ICE") {
       ice.push(dataObj);
-    } else if (splitName[1] === 'POCE') {
+    } else if (splitName[1] === "POCE") {
       poce.push(dataObj);
     } else {
       general.push(dataObj);
     }
   }
-}
- 
+};
+
 // ###################################################################
 // ###################################################################
 
@@ -92,16 +96,16 @@ function Modules({ scheduleData, coursetype }) {
   const scheduleUrls = [];
   const scheduleObjs = [];
 
-  // looking through entire data file 
+  // looking through entire data file
   Object.keys(scheduleData).map((day) => {
-    accessingRequiredItems (
-                            scheduleData, 
-                            day, 
-                            scheduleUrls, 
-                            scheduleObjs, 
-                            moduleNameArray
-                            );
-  })
+    accessingRequiredItems(
+      scheduleData,
+      day,
+      scheduleUrls,
+      scheduleObjs,
+      moduleNameArray
+    );
+  });
 
   moduleNameArray.sort();
 
@@ -118,41 +122,38 @@ function Modules({ scheduleData, coursetype }) {
           // sort each object in scheduleObjs into one of 3 arrays, general, ice, poce
           scheduleObjs.forEach((urlObj) => {
             sortScheduleObjs(urlObj, moduleName, general, ice, poce);
-          })
+          });
 
           const sectionArray = [];
           sectionArray.push(general);
           sectionArray.push(ice);
           sectionArray.push(poce);
 
-          // names of each section 
-          const sectionNames = ['General', 'In Class', 'Post Class'];
+          // names of each section
+          const sectionNames = ["General", "In Class", "Post Class"];
 
           return (
             <>
-            <h6 className="sidebar-subheading">{moduleName}</h6>
-            <Accordion className="modules-accordion">
-            {sectionArray.map((section, index) => {
-              return (
-              <ModuleSection
-                        section={section} 
-                        index={index} 
-                        sectionNames={sectionNames} 
-                        coursetype={coursetype} 
-                        scheduleData={scheduleData} 
-                        />
-              )
-            })}
-            </Accordion>
+              <h6 className="sidebar-subheading">{moduleName}</h6>
+              <Accordion>
+                {sectionArray.map((section, index) => {
+                  return (
+                    <ModuleSection
+                      section={section}
+                      index={index}
+                      sectionNames={sectionNames}
+                      coursetype={coursetype}
+                      scheduleData={scheduleData}
+                    />
+                  );
+                })}
+              </Accordion>
             </>
-            )
-          }
-        )}
+          );
+        })}
       </Nav>
     </div>
   );
 }
 
 export default Modules;
-
-
