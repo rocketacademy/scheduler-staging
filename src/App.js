adaf0c36a2d838c98de5d5894dad82cd95eaf246
neokaiyuan <kai@rocketacademy.co>
2022-06-11 17:02:45 +0800

    Space out BC3.0 schedule by 1 day for M1, add FTBC8 schedule

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter, Switch, Route } from "react-router-dom";
 
 import ftbc6 from "./data/10-01-2022_28-06-2022_FTBC6.json";
 import ftbc7 from "./data/28-03-2022_20-09-2022_FTBC7.json";
+import ftbc8 from "./data/04-07-2022_26-10-2022_FTBC8.json";
 import ptbc1 from "./data/17-08-2021_23-07-2022_PTBC1.json";
 import ptbc2 from "./data/16-11-2021_25-10-2022_PTBC2.json";
 import ptbc4 from "./data/14-05-2022_07-01-2023_PTBC4.json";
@@ -11,10 +12,11 @@ import ScheduleContainer from "./components/schedules/ScheduleContainer";
 import BatchScheduleGenerator from "./components/BatchScheduleGenerator";
 
 function App() {
-  // Create array of batch data for components for /schedule and /admin
+  // Schedule data for each batch
   const batchArray = [
     { name: "ftbc6", content: ftbc6 },
     { name: "ftbc7", content: ftbc7 },
+    { name: "ftbc8", content: ftbc8 },
     { name: "ptbc1", content: ptbc1 },
     { name: "ptbc2", content: ptbc2 },
     { name: "ptbc4", content: ptbc4 },
