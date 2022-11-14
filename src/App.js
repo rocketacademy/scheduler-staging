d7b757e8766e7349acf2a255731c05d0f4dd44ce
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2022-11-14 17:51:52 +0800

    adding PTBC6 schedule

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 
 import ftbc9 from "./data/17-10-2022_23-02-2023_FTBC9.json";
+// uncomment below on december 15th
 // import ftbc10 from "./data/16-01-2023_10-05-2023_FTBC10.json";
 import ptbc6 from "./data/26-11-2022_29-07-2023_PTBC6.json";
 import ptbc4 from "./data/14-05-2022_07-01-2023_PTBC4.json";
@@ -14,6 +15,7 @@ function App() {
   // Schedule data for each batch
   const batchArray = [
     { name: "ftbc9", content: ftbc9 },
+    // uncomment below on dec 15th
     // { name: "ftbc10", content: ftbc10 },
     { name: "ptbc4", content: ptbc4 },
     { name: "ptbc5", content: ptbc5 },
