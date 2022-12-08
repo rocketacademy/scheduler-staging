f0524274e77bd95e071a314ba350fcc190de8b9d
Sam O'Shaughnessy <samoshaughnessy123@hotmail.com>
2022-12-08 15:55:42 +0800

    testing schedule

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 
 import ftbc9 from "./data/17-10-2022_23-02-2023_FTBC9.json";
-// import ftbc10 from "./data/30-01-2023_22-05-2023_FTBC10.json";
+import ftbc10 from "./data/30-01-2023_22-05-2023_FTBC10.json";
 
 import ptbc4 from "./data/14-05-2022_07-01-2023_PTBC4.json";
 import ptbc5 from "./data/30-08-2022_29-04-2023_PTBC5.json";
@@ -14,7 +14,7 @@ import BatchScheduleGenerator from "./components/BatchScheduleGenerator";
 function App() {
   const batchArray = [
     { name: "ftbc9", content: ftbc9 },
-    // { name: "ftbc10", content: ftbc10 },
+    { name: "ftbc10", content: ftbc10 },
     { name: "ptbc4", content: ptbc4 },
     { name: "ptbc5", content: ptbc5 },
     { name: "ptbc6", content: ptbc6 },
